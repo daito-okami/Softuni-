91a39d52b12abab91427d4633a334be307f2303d
daito <daito.okami@gmail.com>
2021-02-02 13:09:59 +0200

    Added Exercise

---
+++ Programming- Python-Advanced/Functions_Advanced_Exercise/Even_or_odd.py
@@ -0,0 +1,5 @@
+def even_odd(*args,):
+    command = args[-1]
+    if command == "even":
+        return list(filter(lambda x: x % 2 == 0, args[0:-1]))
+    return list(filter(lambda x: x % 2 != 0, args[0:-1]))
