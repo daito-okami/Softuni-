e164db37cb158cc1ed7da28a5d6ab8f6bbcce5e1
daito <daito.okami@gmail.com>
2021-03-06 20:17:04 +0200

    Added Lab

---
+++ Programming-Python-OOP/classes_and_instances_lab/vet.py
@@ -0,0 +1,29 @@
+class Vet:
+    animals = []
+    space = 5
+
+    def __init__(self, name):
+        self.name = name
+        self.animals = []
+
+    def register_animal(self, animal_name):
+        if self.space <= len(self.animals):
+            return "Not enough space"
+
+        Vet.animals.append(animal_name)
+        self.animals.append(animal_name)
+        return f"{animal_name} registered in the clinic"
+
+    def unregister_animal(self, animal_name):
+        if animal_name not in self.animals:
+            return f'{animal_name} registered in the clinic'
+
+        self.animals.remove(animal_name)
+        Vet.animals.remove(animal_name)
+
+        return f"{animal_name} unregistered successfully"
+
+    def info(self):
+        animals_count = len(self.animals)
+        space_left = Vet.space - animals_count
+        return f"{self.name} has {animals_count} animals. {space_left} space left in clinic"
