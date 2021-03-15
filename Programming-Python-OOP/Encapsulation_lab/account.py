78945c4673db22b49a073a09d50ee9eb81351c38
daito <daito.okami@gmail.com>
2021-03-15 21:02:15 +0200

    Added LAB

---
+++ Programming-Python-OOP/Encapsulation_lab/account.py
@@ -0,0 +1,16 @@
+class Account:
+    def __init__(self, id, balance, pin):
+        self.__id = id
+        self.balance = balance
+        self.__pin = pin
+
+    def get_id(self, pin):
+        if not self.__pin == pin:
+            return "Wrong pin"
+        return self.__id
+
+    def change_pin(self, old_pin, new_pin):
+        if not old_pin == self.__pin:
+            return "Wrong pin"
+        self.__pin = new_pin
+        return "Pin changed"
