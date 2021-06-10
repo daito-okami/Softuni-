4b0072645e63e69ba79fbf6d9870014ddc6ec1bc
daito <daito.okami@gmail.com>
2021-06-11 00:09:49 +0300

    http lesson

--- Programming-Web-Basics/djangoProject/djangoProject/cities/views.py
+++ Programming-Web-Basics/djangoProject/djangoProject/cities/views.py
@@ -1,18 +1,28 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from djangoProject.cities.models import Person
 
 
 def index(req):
     context = {'name': 'Daito',
-               'people':Person.objects.all(),
-    }
+               'people': Person.objects.all(),
+               }
     return render(req, 'index.html', context)
 
+
+def create_person(req):
+    Person(
+        name="Pesho",
+        age=11,
+        home_town='sofia'
+    ).save()
+
+    return redirect('/cities')
 
 
 def list_phones(request):
     context = {'phones': ['Galaxy s5000', 'Iphone 12', 'Huawei Y7 Prime', ], 'message': 'Phones list'}
     return render(request, 'phones.html', context)
+
     # return HttpResponse('Phones list')
--- Programming-Web-Basics/djangoProject/djangoProject/urls.py
+++ Programming-Web-Basics/djangoProject/djangoProject/urls.py
@@ -2,10 +2,11 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from djangoProject.cities.views import index, list_phones
+from djangoProject.cities.views import index, list_phones, create_person
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('create/', create_person, name="create person"),
     path('cities/', include('djangoProject.cities.urls')),
     path('', include('djangoProject.people.urls')),
 
