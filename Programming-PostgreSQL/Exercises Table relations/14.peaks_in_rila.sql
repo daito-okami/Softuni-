e0e4f52bd6c05f7f5be1ca821c5caf675ca1c48a
daito <daito.okami@gmail.com>
2023-10-01 15:26:25 +0300

    Added Exercise

---
+++ Programming-PostgreSQL/Exercises Table relations/14.peaks_in_rila.sql
@@ -0,0 +1,15 @@
+SELECT
+	mountain_range,
+	peak_name,
+	elevation
+FROM 
+	peaks
+JOIN
+	mountains
+ON
+	mountain_id = mountains.id
+	
+WHERE 
+	mountain_range LIKE '%Rila%'
+ORDER BY
+	elevation DESC;
